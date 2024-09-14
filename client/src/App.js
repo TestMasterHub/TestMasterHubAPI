9748dd686aaeff7877014de715403149b29dee99
Meesala Vineel Sai <85602650+VineelSaiMeesala@users.noreply.github.com>
2024-09-14 13:45:17 +0530

    removed __Tests__ from Git
    removed __Tests__ from Git as test only for local R&D purpose

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 // import Homepage from './Pages/Homepage';
-import CollectionPage from './Pages/CollectionPage';
-// import CollectionPage from './__Tests__/CollectionPage2_O';
+// import CollectionPage from './Pages/CollectionPage';
+import CollectionPage from './__Tests__/CollectionPage2_O';
 function App() {
   return (
     <div className="App">
